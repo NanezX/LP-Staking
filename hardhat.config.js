c1b4826b85907b261fb55ebdbb6a91da40c4a81a
NanezX <victor.nanezj@gmail.com>
2021-06-22 16:23:59 -0400

    Init

---
+++ hardhat.config.js
@@ -0,0 +1,10 @@
+require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-ethers');
+require('@openzeppelin/hardhat-upgrades');
+require("hardhat-gas-reporter");
+require('dotenv').config();
+
+module.exports = {
+  solidity: "0.8.4",
+};
+
