9007416da5af7007aaa66cad3e117ddf06df016d
NanezX <victor.nanezj@gmail.com>
2021-06-22 19:26:09 -0400

    Added UniswapV2 as dep

---
+++ contracts/StakeLP.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract StakeLP {
+
+    function StakeETH () public payable {
+
+    }
+
+    function addLiquidity() public {
+
+    }
+}
